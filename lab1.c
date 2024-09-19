e61179af99f847ea38632c2b8a7c1e83ff05d2bd
user <user@ws1013402137.pm>
2024-09-19 13:53:44 +0500

    first commit

---
+++ lab1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c;
+    suma = a
+    c = 0
+    while(suma!=0){
+        c+= suma%10;
+        suma/=10
+    }
+    if (suma = b)
+        printf("Сумма цифр =",b)
+    if (suma>b)
+        printf(suma)
+    else
+        printf(suma+b)
+}
